3808989d615705d9ac5d573cd300c0b98804191c
tahlilislam <tahlilislam@gmail.com>
2024-01-07 21:45:35 -0800

    steps 1,2 ,3...still need to fix bugs

--- db.js
+++ db.js
@@ -1,2 +1,15 @@
 /** Database setup for BizTime. */
 
+/** Database setup for BizTime. */
+
+
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: "postgresql:///biztime"
+});
+
+client.connect();
+
+
+module.exports = client;
